267be1dc1d11bb8d6a59cc8b49c209461ff9964a
Chandrika <chandrika.mjee@gmail.com>
2021-09-29 03:08:49 -0400

    adding problem2

--- lab2/v2/client.c
+++ lab2/v2/client.c
@@ -9,35 +9,48 @@
 #include <netinet/in.h>
 #include <time.h>
 #include<signal.h>
+#include <stdbool.h>
 
-#define MAX 100
+#define MAX 4096
 #define SA struct sockaddr
+void send_receive_request();
+void term_prog (int sig);
 
-int main(int argc, char* argv[]){
+int count_timer;
 
-	if(argc<3){
-		printf("%s\n","please provide all the inputs" );
-		return 0;
-	}
 
-	int sock_desc, connfd;
-    struct sockaddr_in servaddr, cliaddr;
-   
-    // socket create and varification
-    sock_desc = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock_desc == -1) {
-        printf("socket creation failed...\n");
-        exit(0);
-    }
-    else
-        printf("Socket successfully created..\n");
-    bzero(&servaddr, sizeof(servaddr));
-   
+char buff[MAX];
+char output[MAX];
+char* prt[1];
+char* ipc[1];
+bool flag;
+int sock_desc, connfd;
+struct sockaddr_in servaddr, cliaddr;
+struct sigaction sact;
+
+void initiate_socket(){
+	 sock_desc = socket(AF_INET, SOCK_STREAM, 0);
+	    if (sock_desc == -1) {
+	        printf("socket creation failed...\n");
+	        exit(0);
+	    }
+	    else
+	        printf("Socket successfully created..\n");
+}
+
+void close_socket(){
+	close(sock_desc);
+}
+
+void connect_to_socket(){
+	bzero(&servaddr, sizeof(servaddr));
+	   
     // assign IP, PORT
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr(argv[1]);
-    servaddr.sin_port = htons(atoi(argv[2]));
+    servaddr.sin_addr.s_addr = inet_addr(ipc[0]);
+    servaddr.sin_port = htons(atoi(prt[0]));
    
+
     // connect the client socket to server socket
     if (connect(sock_desc, (SA*)&servaddr, sizeof(servaddr)) != 0) {
         printf("connection with the server failed...\n");
@@ -45,20 +58,91 @@ int main(int argc, char* argv[]){
     }
     else
         printf("connected to the server..\n");
-   
-   
-    char buff[MAX];
-    while(1){
-    	bzero(buff, sizeof(buff));
-    	printf("Enter the command  : ");
-    	fgets(buff, MAX, stdin);
-        buff[strlen(buff)-1]='\0';
-        write(sock_desc, buff, sizeof(buff));
-        bzero(buff, sizeof(buff));
-        read(sock_desc, buff, sizeof(buff));
-        printf("From Server : %s", buff);
+}
+
+void establish_connection_with_server(){
+	close_socket();
+	initiate_socket();
+	connect_to_socket();
+}
+
+
+void term_prog (int sig) {
+	printf("didn't recieve the response from server\n");
+	if(count_timer==0)
+	{
+		printf("Terminating Client Request Process \n");
+    	kill (0,SIGTERM);
+	}
+    else{
+    	
+    	printf("counter timer = %d\n",count_timer );
+    	count_timer--;
+    	close_socket();
+
+
+    	
     }
-   
-    // close the socket
-    close(sock_desc);
+
+
+}
+
+
+
+int main(int argc, char* argv[]){
+	count_timer=3;
+	flag = false;
+
+	
+
+
+	if(argc<3){
+		printf("%s\n","please provide all the inputs" );
+		return 0;
+	}
+
+	ipc[0] = argv[1];
+	prt[0] = argv[2];
+
+	printf("Enter the command  : ");
+    bzero(buff, sizeof(buff));
+    fgets(buff, MAX, stdin);
+    buff[strlen(buff)-1]='\0';
+
+
+	while(1){
+		printf("Client Connection Starts\n");
+		initiate_socket();
+	    connect_to_socket();
+
+		//establish_connection_with_server();
+	   
+	    // getting input from client
+	   	
+	    
+
+		bzero(output, sizeof(output));
+	    //signal(SIGALRM, term_prog);
+	   
+	    write(sock_desc, buff, sizeof(buff));
+	    printf("writing done \n");
+
+	    struct sigaction action;
+  		action.sa_handler = term_prog;
+  		action.sa_flags = SA_RESTART; //<-- restart 
+  		sigaction(SIGALRM, &action, NULL);
+	    alarm(2);
+
+	    read(sock_desc, output, sizeof(output));
+	    
+	    close_socket();
+	    if(strcmp(output,"")!=0)
+	    {
+	    	printf("hi From Server : %s\n", output);
+	    	kill(0,SIGTERM);
+	    }
+
+	    printf("********************************\n\n\n");
+	    //kill (0,SIGTERM);
+	}
 }
--- lab2/v2/server.c
+++ lab2/v2/server.c
@@ -9,10 +9,47 @@
 #include <netinet/in.h>
 #include <time.h>
 #include<signal.h>
-#define MAX 100
+#define MAX 4096
 #define SA struct sockaddr
 
 char* token[100];
+
+int security_check(struct sockaddr_in cliaddr, char buf[MAX]){
+
+	char* ip = inet_ntoa(cliaddr.sin_addr);
+
+	char* tk[100];
+	char dm[2]=".";
+
+	int i=0;
+	tk[0]=strtok(ip,dm);
+	while(tk[i]!=NULL){
+		i++;
+		tk[i]=strtok(NULL,dm);
+	}
+
+	if(!(strcmp(tk[0],"127")==0 && strcmp(tk[1],"0")==0 && (strcmp(tk[2],"0")==0 || strcmp(tk[2],"1")==0)))
+	{
+		printf("ip address invalid \n");
+		return -1;
+	}
+
+	printf("IP address check successful \n");
+
+	if(strcmp(buf,"date")==0 || strcmp(buf,"/bin/date")==0){
+		printf("message is secure  \n");
+		return 1;
+	}
+
+	else
+	{
+		printf("Message is unsecured\n");
+		return -1;
+	}
+
+	return -1;
+
+}
 void  parse(char buf[100]){
 
 	int k=0;
@@ -83,7 +120,7 @@ int main(int argc, char *argv[]){
 
 	    bzero(&cliaddr, sizeof(cliaddr));
 	    len = sizeof(cliaddr);
-	   
+	   	printf("\n\n*****************\n");
 	    
 	    connfd = accept(socket_desc, (SA*)&cliaddr, &len);
 	    if (connfd < 0) {
@@ -91,14 +128,13 @@ int main(int argc, char *argv[]){
 	        exit(0);
 	    }
 	    else
-	        printf("server accept the client...\n");
+	        printf("server accepted the client...\n");
 	   
 	    
 	    time_t t;
 	    srand((unsigned) time(&t));
-	   // int toss_val = rand()%2;
-	    int toss_val = 1;
-
+	    int toss_val = rand()%2;
+	    //int toss_val =0;
 	    printf("value of toss is = %d\n",toss_val);
 
 	    // head comes
@@ -108,33 +144,49 @@ int main(int argc, char *argv[]){
 	    }
 	    else{
 	    	printf("Tail came, processing the request \n");
+
+
 	    	bzero(buff, MAX);
    
         	// read the message from client and copy it in buffer
         	read(connfd, buff, sizeof(buff));
+
 	    	// tails come
 	    	printf("Received message from IP: %s and port: %i\n",
 	           inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
 
 	    	printf("Client Message : %s\n", buff);
+
+	    	int res = security_check(cliaddr,buff);
+
+	    	if(res==1){
 	    	
 
+	    		printf("Security check is successful \n");
+		    	parse(buff);
 
-	    	parse(buff);
-	    	
-	    	pid_t frk;
-	    	int status;
-	    	frk = fork();
-	    	
-	    	
-	    	if(frk==0){
+		    	pid_t frk;
+		    	int status;
+		    	
+		    	frk = fork();
+		    	
+		    	
+		    	if(frk==0){
+		    		dup2(connfd,1);
+		    		if(execvp(token[0],token) == -1)
+		    		{
+		    			fprintf(stderr, "%s\n", "server can't process the request");
+		                exit(1);
+		    		}
 
-	    		if(execvp(token[0],token) == -1)
-	  	    		exit(1);
-
+		    	}
+		    	else{
+		    		waitpid(frk, &status, 0);
+		    		printf("child exited \n");
+		    	}
 	    	}
 	    	else{
-	    		waitpid(frk, &status, 0);
+	    		printf("Server can't process unsecured request\n");
 	    	}
 	    }
 	}
